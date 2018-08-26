d8107d77f40058d9e56213d62adab681b9b7ee22
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2018-08-26 13:43:25 +0000

    Missing conversion message
    git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@1614 111d7451-93fa-4e81-90d4-8039f76616e9

--- gapobj.c
+++ gapobj.c
@@ -44,8 +44,7 @@ static MUST_CHECK Obj *create(Obj *v1, linepos_t epoint) {
     case T_GAP: return val_reference(v1);
     default: break;
     }
-    err_msg_wrong_type(v1, NULL, epoint);
-    return (Obj *)ref_none();
+    return (Obj *)new_error_conv(v1, GAP_OBJ, epoint);
 }
 
 static FAST_CALL bool same(const Obj *o1, const Obj *o2) {
