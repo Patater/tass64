c1b8b5e8bc26429a2f0104474ce162d0c1fc45bc
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2018-12-08 09:55:54 +0000

    Simplify
    git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@1673 111d7451-93fa-4e81-90d4-8039f76616e9

--- error.c
+++ error.c
@@ -41,6 +41,7 @@
 #include "typeobj.h"
 #include "labelobj.h"
 #include "errorobj.h"
+#include "noneobj.h"
 
 #ifdef COLOR_OUTPUT
 bool print_use_color = false;
@@ -720,6 +721,16 @@ void err_msg_wrong_type(const Obj *val, Type *expected, linepos_t epoint) {
         adderror(expected->name);
     }
     adderror("'");
+}
+
+void err_msg_wrong_type2(const Obj *val, Type *expected, linepos_t epoint) {
+    if (val->obj == ADDRESS_OBJ) {
+        const Obj *val2 = ((Address *)val)->val;
+        if (val2->obj == ERROR_OBJ || val2->obj == NONE_OBJ) val = val2;
+    }
+    if (val->obj == ERROR_OBJ) err_msg_output((Error *)val);
+    else if (val->obj == NONE_OBJ) err_msg_still_none(NULL, epoint);
+    else err_msg_wrong_type(val, expected, epoint);
 }
 
 void err_msg_cant_unpack(size_t expect, size_t got, linepos_t epoint) {
