71cf4d5a052c7434367ccf9de4a1af280f315c18
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2018-12-23 17:34:58 +0000

    Break cycles
    git-svn-id: https://svn.code.sf.net/p/tass64/code/trunk@1726 111d7451-93fa-4e81-90d4-8039f76616e9

--- namespaceobj.c
+++ namespaceobj.c
@@ -106,11 +106,13 @@ static FAST_CALL void garbage(Obj *o1, int i) {
 
 static FAST_CALL bool same(const Obj *o1, const Obj *o2) {
     const Namespace *v1 = (const Namespace *)o1, *v2 = (const Namespace *)o2;
-    const struct avltree_node *n;
-    const struct avltree_node *n2;
+    const struct avltree_node *n, *n2;
+    struct avltree_node *f, *f2;
     if (o2->obj != NAMESPACE_OBJ) return false;
-    n = avltree_first(&v1->members);
-    n2 = avltree_first(&v2->members);
+    n = f = avltree_first(&v1->members);
+    n2 = f2 = avltree_first(&v2->members);
+    ((Namespace *)v1)->members.first = NULL;
+    ((Namespace *)v2)->members.first = NULL;
     while (n != NULL && n2 != NULL) {
         const struct namespacekey_s *p, *p2;
         for (;;) {
@@ -133,10 +135,16 @@ static FAST_CALL bool same(const Obj *o1, const Obj *o2) {
         }
         if (n == NULL || n2 == NULL) break;
         if ((p->key == NULL) != (p2->key == NULL)) return false;
-        if (p->key != NULL && p2->key != NULL && !p->key->v.obj->same((Obj *)p->key, (Obj *)p2->key)) return false;
+        if (p->key != NULL && p2->key != NULL && !p->key->v.obj->same((Obj *)p->key, (Obj *)p2->key)) {
+            ((Namespace *)v1)->members.first = f;
+            ((Namespace *)v2)->members.first = f2;
+            return false;
+        }
         n = avltree_next(n);
         n2 = avltree_next(n2);
     }
+    ((Namespace *)v1)->members.first = f;
+    ((Namespace *)v2)->members.first = f2;
     return n == n2;
 }
 
