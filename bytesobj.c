dee59e851169fad5e36c04c3b94645e83993a439
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2018-12-31 21:31:45 +0000

    Not needed
    git-svn-id: https://svn.code.sf.net/p/tass64/code/trunk@1763 111d7451-93fa-4e81-90d4-8039f76616e9

--- bytesobj.c
+++ bytesobj.c
@@ -1213,7 +1213,6 @@ static MUST_CHECK Obj *slice(Obj *o1, oper_t op, size_t indx) {
                 offs += step;
             }
         }
-        if (length > SSIZE_MAX) goto failed2; /* overflow */
         v->len = (ssize_t)length;
         return &v->v;
     }
