20263c17bb08ffe663b3fbf8aee6694d52a3c3dd
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2019-01-13 20:49:04 +0000

    Hash for bits too...
    git-svn-id: https://svn.code.sf.net/p/tass64/code/trunk@1822 111d7451-93fa-4e81-90d4-8039f76616e9

--- bitsobj.c
+++ bitsobj.c
@@ -750,6 +750,7 @@ static MUST_CHECK Obj *calc1(oper_t op) {
     case O_INV: 
         if (op->inplace != &v1->v) return invert(v1, op->epoint3);
         v1->len = ~v1->len;
+        if (v1->data != v1->u.val) v1->u.hash = -1;
         return val_reference(&v1->v);
     case O_POS: return val_reference(&v1->v);
     case O_NEG:
