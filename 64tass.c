e93fe0c97f3f87fef41d361482e15e0d98f3508e
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2019-02-22 18:25:42 +0000

    Only copy if really needed
    git-svn-id: https://svn.code.sf.net/p/tass64/code/trunk@1901 111d7451-93fa-4e81-90d4-8039f76616e9

--- 64tass.c
+++ 64tass.c
@@ -1461,9 +1461,13 @@ static size_t for_command(Label *newlabel, List *lst, linepos_t epoint) {
         line_t xlin = lpoint.line;
         struct oper_s tmp;
         const uint8_t *oldpline = pline;
-        size_t lentmp = strlen((const char *)pline) + 1;
-        expr = (uint8_t *)mallocx(lentmp);
-        memcpy(expr, pline, lentmp); label = NULL;
+        if ((size_t)(pline - current_file_list->file->data) >= current_file_list->file->len) {
+            size_t lentmp = strlen((const char *)pline) + 1;
+            expr = (uint8_t *)mallocx(lentmp);
+            memcpy(expr, pline, lentmp);
+            pline = expr;
+        } else expr = (uint8_t *)oldpline;
+        label = NULL;
         new_waitfor(W_NEXT2, epoint);
         waitfor->u.cmd_rept.breakout = false;
         tmp.op = NULL;
@@ -1589,6 +1593,7 @@ static size_t for_command(Label *newlabel, List *lst, linepos_t epoint) {
             }
         }
         pline = oldpline;
+        if (expr == oldpline) expr = NULL;
         lpoint.line = xlin;
     }
     if (nf != NULL) {
