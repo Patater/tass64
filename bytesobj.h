6fb54496f8542cee714912a64b1972fddd303c3f
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2019-08-25 05:25:43 +0000

    Convert directly
    git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@1923 111d7451-93fa-4e81-90d4-8039f76616e9

--- bytesobj.h
+++ bytesobj.h
@@ -59,11 +59,13 @@ static inline Bytes *ref_bytes(Bytes *v1) {
 extern MALLOC Bytes *new_bytes(size_t);
 
 struct Str;
+struct Bits;
 
 extern MUST_CHECK Bytes *bytes_from_u8(unsigned int);
 extern MUST_CHECK Bytes *bytes_from_u16(unsigned int);
 extern MUST_CHECK Bytes *bytes_from_uval(uval_t, unsigned int);
 extern MUST_CHECK Obj *bytes_from_str(const struct Str *, linepos_t, Textconv_types);
+extern MUST_CHECK Obj *bytes_from_bits(const struct Bits *, linepos_t);
 extern MUST_CHECK Obj *bytes_from_hexstr(const uint8_t *, size_t *, linepos_t);
 extern MUST_CHECK Obj *bytes_from_z85str(const uint8_t *, size_t *, linepos_t);
 extern MUST_CHECK Obj *float_from_bytes(const Bytes *, linepos_t);
