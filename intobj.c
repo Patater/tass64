bc044f3c4387129f74469c3074e705da560716d1
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2019-09-01 13:05:32 +0000

    Fast compare
    git-svn-id: https://svn.code.sf.net/p/tass64/code/trunk@1955 111d7451-93fa-4e81-90d4-8039f76616e9

--- intobj.c
+++ intobj.c
@@ -134,7 +134,12 @@ static MUST_CHECK Int *return_int(digit_t c, bool neg) {
 static FAST_CALL bool same(const Obj *o1, const Obj *o2) {
     const Int *v1 = (const Int *)o1, *v2 = (const Int *)o2;
     if (o2->obj != INT_OBJ || v1->len != v2->len) return false;
-    return memcmp(v1->data, v2->data, intlen(v1) * sizeof *v1->data) == 0;
+    switch (v1->len) {
+    case 0: return true;
+    case -1:
+    case 1: return v1->data[0] == v2->data[0];
+    default: return memcmp(v1->data, v2->data, intlen(v1) * sizeof *v1->data) == 0;
+    }
 }
 
 static MUST_CHECK Obj *truth(Obj *o1, Truth_types type, linepos_t epoint) {
