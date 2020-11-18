6ee9aace4ea02171eea34c4a75fe0224651f6c87
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2020-11-18 20:27:10 +0000

    Add --no-output
    git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@2261 111d7451-93fa-4e81-90d4-8039f76616e9

--- mem.c
+++ mem.c
@@ -482,6 +482,7 @@ void output_mem(Memblocks *memblocks, const struct output_s *output) {
     memcomp(memblocks, output->mode == OUTPUT_XEX || output->mode == OUTPUT_IHEX || output->mode == OUTPUT_SREC);
 
     if (memblocks->mem.p == 0) return;
+    if (output->name == NULL) return;
 
     if (dash_name(output->name)) {
 #if defined _WIN32 || defined __DJGPP__
