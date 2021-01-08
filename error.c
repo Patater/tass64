a3481f591daf4113d44ab8fb2246283f20f5c1dd
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2021-01-08 23:05:23 +0000

    Remove -Mswitch-case due to unwanted side-effects
    It's not possible to do this check properly without causing possible
    side-effect in some cases
    
    
    git-svn-id: https://svn.code.sf.net/p/tass64/code/trunk@2269 111d7451-93fa-4e81-90d4-8039f76616e9

--- error.c
+++ error.c
@@ -466,10 +466,6 @@ void err_msg2(Error_types no, const void *prm, linepos_t epoint) {
             new_error_msg2(diagnostic_errors.old_equal, epoint);
             adderror("deprecated equal operator, use '==' instead [-Wold-equal]");
             break;
-        case ERROR_DUPLICATECASE:
-            new_error_msg2(diagnostic_errors.switch_case, epoint);
-            adderror("case ignored, value already handled [-Wswitch-case]");
-            break;
         case ERROR_NONIMMEDCONST:
             new_error_msg2(diagnostic_errors.immediate, epoint);
             adderror("immediate addressing mode suggested [-Wimmediate]");
