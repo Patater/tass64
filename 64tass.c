55f66803983c8f1b54201b1d7c3accfdccb36175
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2021-01-09 16:32:14 +0000

    Only half of it...
    git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@2272 111d7451-93fa-4e81-90d4-8039f76616e9

--- 64tass.c
+++ 64tass.c
@@ -2306,6 +2306,7 @@ MUST_CHECK Obj *compile(void)
                             Mfunc *mfunc;
                             bool labelexists;
                             listing_line(listing, 0);
+                            new_waitfor(W_ENDF, &cmdpoint);
                             label = new_label(&labelname, mycontext, strength, &labelexists, current_file_list);
                             mfunc = (Mfunc *)val_alloc(MFUNC_OBJ);
                             mfunc->file_list = current_file_list;
@@ -2370,7 +2371,7 @@ MUST_CHECK Obj *compile(void)
                                 mfunc->names = new_namespace(current_file_list, &epoint);
                                 waitfor->u.cmd_function.val = val_reference(&mfunc->v);
                             }
-                            new_waitfor(W_ENDF, &cmdpoint);waitfor->skip = 0;
+                            waitfor->skip = 0;
                             goto finish;
                         }
                     case CMD_STRUCT:
