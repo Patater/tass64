ba1d1247e04c9b906fb8c51321d0d5d04e6f9937
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2021-04-25 21:09:11 +0000

    1.56.2625
    git-svn-id: https://svn.code.sf.net/p/tass64/code/trunk@2625 111d7451-93fa-4e81-90d4-8039f76616e9

--- 64tass.h
+++ 64tass.h
@@ -23,10 +23,10 @@
 #include "inttypes.h"
 #include "wait_e.h"
 #ifndef REVISION
-#define REVISION "2200?"
+#define REVISION "2625?"
 #endif
 #undef VERSION
-#define VERSION "1.55." REVISION
+#define VERSION "1.56." REVISION
 #define MAX_PASS 20
 
 #define ignore() while(pline[lpoint.pos]==0x20 || pline[lpoint.pos]==0x09) lpoint.pos++
