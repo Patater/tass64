Patater/tass64

commit 02fb3c2ebc091861dd5f583acc94db9d615cba0c

Fix warnings
git-svn-id: https://svn.code.sf.net/p/tass64/code/trunk@2428 111d7451-93fa-4e81-90d4-8039f76616e9

codeobj.c
@@ -115,12 +115,13 @@ static FAST_CALL bool same(const Obj *o1, const Obj *o2) {
 }
 
 static inline address_t code_address(const Code *v1) {
-    return (v1->addr + v1->offs) & all_mem;
+    return v1->offs < 0 ? v1->addr - (uval_t)-v1->offs : v1->addr + (uval_t)v1->offs;
 }
 
 static MUST_CHECK Obj *get_code_address(const Code *v1, linepos_t epoint) {
-    address_t addr = code_address(v1);
-    if (v1->addr + v1->offs != addr) err_msg_addr_wrap(epoint);
+    address_t addr2 = code_address(v1);
+    address_t addr = addr2 & all_mem;
+    if (addr2 != addr) err_msg_addr_wrap(epoint);
     return get_star_value(addr, v1->typ);
 }
 
@@ -135,7 +136,7 @@ MUST_CHECK Error *code_uaddress(Obj *o1, uval_t *uv, uval_t *uv2, linepos_t epoi
     Code *v1 = (Code *)o1;
     Error *v = access_check(v1, epoint);
     if (v != NULL) return v;
-    *uv = v1->addr + v1->offs;
+    *uv = code_address(v1);
     *uv2 = v1->addr;
     if (bits >= sizeof(*uv2)*8 || (*uv2 >> bits) == 0) {
         return NULL;
@@ -165,7 +166,7 @@ static MUST_CHECK Obj *repr(Obj *o1, linepos_t epoint, size_t maxsize) {
         Error *err = access_check(v1, epoint);
         if (err != NULL) return &err->v;
         v = get_code_address(v1, epoint);
-    } else v = get_star_value(code_address(v1), v1->typ);
+    } else v = get_star_value(code_address(v1) & all_mem, v1->typ);
     result = v->obj->repr(v, epoint, maxsize);
     val_destroy(v);
     return result;
@@ -178,7 +179,7 @@ static MUST_CHECK Obj *str(Obj *o1, linepos_t epoint, size_t maxsize) {
         Error *err = access_check(v1, epoint);
         if (err != NULL) return &err->v;
         v = get_code_address(v1, epoint);
-    } else v = get_star_value(code_address(v1), v1->typ);
+    } else v = get_star_value(code_address(v1) & all_mem, v1->typ);
     result = v->obj->str(v, epoint, maxsize);
     val_destroy(v);
     return result;
@@ -186,13 +187,14 @@ static MUST_CHECK Obj *str(Obj *o1, linepos_t epoint, size_t maxsize) {
 
 static MUST_CHECK Error *iaddress(Obj *o1, ival_t *iv, unsigned int bits, linepos_t epoint) {
     Code *v1 = (Code *)o1;
-    address_t addr;
+    address_t addr, addr2;
     Error *v = access_check(v1, epoint);
     if (v != NULL) return v;
-    addr = code_address(v1);
+    addr2 = code_address(v1);
+    addr = addr2 & all_mem;
     *iv = (ival_t)addr;
     if ((addr >> (bits - 1)) == 0) {
-        if (v1->addr + v1->offs != addr) err_msg_addr_wrap(epoint);
+        if (addr2 != addr) err_msg_addr_wrap(epoint);
         return NULL;
     }
     v = new_error(ERROR_____CANT_IVAL, epoint);
@@ -203,13 +205,14 @@ static MUST_CHECK Error *iaddress(Obj *o1, ival_t *iv, unsigned int bits, linepo
 
 static MUST_CHECK Error *uaddress(Obj *o1, uval_t *uv, unsigned int bits, linepos_t epoint) {
     Code *v1 = (Code *)o1;
-    address_t addr;
+    address_t addr, addr2;
     Error *v = access_check(v1, epoint);
     if (v != NULL) return v;
-    addr = code_address(v1);
+    addr2 = code_address(v1);
+    addr = addr2 & all_mem;
     *uv = addr;
     if (bits >= sizeof(addr)*8 || (addr >> bits) == 0) {
-        if (v1->addr + v1->offs != addr) err_msg_addr_wrap(epoint);
+        if (addr2 != addr) err_msg_addr_wrap(epoint);
         return NULL;
     }
     v = new_error(ERROR_____CANT_UVAL, epoint);
@@ -222,7 +225,7 @@ static MUST_CHECK Error *ival(Obj *o1, ival_t *iv, unsigned int bits, linepos_t 
     Code *v1 = (Code *)o1;
     if (v1->typ->obj->address(v1->typ) != A_NONE) {
         Error *v = new_error(ERROR______CANT_INT, epoint);
-        v->u.obj = get_star_value(code_address(v1), v1->typ);
+        v->u.obj = get_star_value(code_address(v1) & all_mem, v1->typ);
         return v;
     }
     return iaddress(o1, iv, bits, epoint);
@@ -232,7 +235,7 @@ static MUST_CHECK Error *uval(Obj *o1, uval_t *uv, unsigned int bits, linepos_t 
     Code *v1 = (Code *)o1;
     if (v1->typ->obj->address(v1->typ) != A_NONE) {
         Error *v = new_error(ERROR______CANT_INT, epoint);
-        v->u.obj = get_star_value(code_address(v1), v1->typ);
+        v->u.obj = get_star_value(code_address(v1) & all_mem, v1->typ);
         return v;
     }
     return uaddress(o1, uv, bits, epoint);
@@ -477,8 +480,9 @@ static MUST_CHECK Obj *slice(oper_t op, size_t indx) {
 }
 
 static inline address_t ldigit(Code *v1, linepos_t epoint) {
-    address_t addr = code_address(v1);
-    if (v1->addr + v1->offs != addr) err_msg_addr_wrap(epoint);
+    address_t addr2 = code_address(v1);
+    address_t addr = addr2 & all_mem;
+    if (addr2 != addr) err_msg_addr_wrap(epoint);
     return addr;
 }
 
@@ -503,7 +507,7 @@ static MUST_CHECK Obj *calc1(oper_t op) {
     case O_HWORD:
     case O_WORD:
     case O_BSWORD:
-        return bytes_calc1(op->op->op, code_address(v1));
+        return bytes_calc1(op->op->op, code_address(v1) & all_mem);
     case O_STRING:
     case O_INV:
     case O_NEG:
@@ -534,7 +538,7 @@ static MUST_CHECK Obj *calc2(oper_t op) {
                 str_cfcpy(&cf, &v2->name);
                 if (str_cmp(&cf, &of) == 0) {
                     if (diagnostics.case_symbol && str_cmp(&v2->name, &cf) != 0) err_msg_symbol_case(&v2->name, NULL, op->epoint2);
-                    return (Obj *)int_from_uval((v1->memaddr + v1->offs) & all_mem2);
+                    return (Obj *)int_from_uval(code_address(v1) & all_mem2);
                 }
             }
         }
@@ -563,11 +567,9 @@ static MUST_CHECK Obj *calc2(oper_t op) {
             err = access_check(v2, op->epoint2);
             if (err != NULL) return &err->v;
             if (op->op->op == O_SUB) {
-                address_t addr1 = code_address(v1);
-                address_t addr2 = code_address(v2);
-                if (v1->addr + v1->offs != addr1) err_msg_addr_wrap(op->epoint);
-                if (v2->addr + v2->offs != addr2) err_msg_addr_wrap(op->epoint2);
-                return (Obj *)int_from_ival(addr1 - addr2);
+                address_t addr1 = ldigit(v1, op->epoint);
+                address_t addr2 = ldigit(v2, op->epoint2);
+                return (Obj *)int_from_ival((ival_t)addr1 - (ival_t)addr2);
             }
             tmp1 = get_code_address(v1, op->epoint);
             tmp2 = get_code_address(v2, op->epoint2);

64tass.c
@@ -347,7 +347,7 @@ static void reset_waitfor(void) {
     struct waitfor_s dummy;
     struct linepos_s lpos = {0, 0};
     dummy.skip = 1;
-    waitfor_p = (size_t)-1;
+    waitfor_p = (size_t)0 - 1U;
     waitfor = &dummy;
     new_waitfor(W_NONE, &lpos);
 }
@@ -1134,7 +1134,7 @@ static void starhandle(Obj *val, linepos_t epoint, linepos_t epoint2) {
             val_destroy(current_address->l_address_val);
             current_address->l_address_val = get_star_value(0, val);
             val_destroy(val);
-            addr = (address_t)uval & all_mem2;
+            addr = uval & all_mem2;
             if (current_address->address != addr) {
                 current_address->address = addr;
                 memjmp(current_address->mem, current_address->address);
@@ -1142,7 +1142,7 @@ static void starhandle(Obj *val, linepos_t epoint, linepos_t epoint2) {
             return;
         }
         laddr = current_address->l_address;
-        addr = (address_t)uval & all_mem;
+        addr = uval & all_mem;
         if (arguments.tasmcomp) addr = (uint16_t)addr;
         else if (addr >= laddr) {
             addr = (current_address->address + (addr - laddr)) & all_mem2;
@@ -3423,9 +3423,12 @@ MUST_CHECK Obj *compile(void)
                         if (!get_exp(0, 0, 0, NULL)) goto breakerr;
                         if (prm == CMD_PTEXT) {
                             trec.buff[0] = outputeor;
+                            trec.sum = 1; 
                             trec.len = 1;
-                        } else trec.len = 0;
-                        trec.sum = trec.len;
+                        } else {
+                            trec.len = 0; 
+                            trec.len = 0;
+                        }
                         trec.max = SIZE_MAX;
                         trec.prm = prm;
                         trec.error = ERROR__USER_DEFINED;
@@ -3542,7 +3545,7 @@ MUST_CHECK Obj *compile(void)
                                 size_t i, ln = cfile2->len - foffset;
                                 uint8_t *d, *s = cfile2->data + foffset;
                                 if (ln > fsize) ln = fsize;
-                                d = pokealloc((address_t)ln, &epoint);
+                                d = pokealloc(ln, &epoint);
                                 if (outputeor != 0) {
                                     for (i = 0; i < ln; i++) d[i] = s[i] ^ outputeor;
                                 } else memcpy(d, s, ln);
@@ -3558,6 +3561,7 @@ MUST_CHECK Obj *compile(void)
                 {   /* .offs */
                     struct values_s *vs;
                     ival_t ival;
+                    address_t addr;
 
                     if (diagnostics.optimize) cpu_opt_invalidate();
                     listing_line(listing, epoint.pos);
@@ -3569,8 +3573,9 @@ MUST_CHECK Obj *compile(void)
                         current_address->moved = true;
                     }
                     current_address->wrapwarn = false;
-                    if (current_address->address != ((address_t)(current_address->l_address + ival) & all_mem2)) {
-                        current_address->address = (address_t)(current_address->l_address + ival) & all_mem2;
+                    addr = (ival < 0 ? current_address->l_address - (uval_t)-ival : current_address->l_address + (uval_t)ival) & all_mem2;
+                    if (current_address->address != addr) {
+                        current_address->address = addr;
                         memjmp(current_address->mem, current_address->address);
                     }
                 }
@@ -3797,8 +3802,8 @@ MUST_CHECK Obj *compile(void)
                             if (uval > max) {
                                 if (itt != 0) db = max - itt + 1;
                             } else {
-                                address_t rem = itt % (address_t)uval;
-                                if (rem != 0) db = (address_t)uval - rem;
+                                address_t rem = itt % uval;
+                                if (rem != 0) db = uval - rem;
                             }
                         }
                     } else {

bitsobj.c
@@ -203,7 +203,7 @@ static FAST_CALL NO_INLINE bool bits_same(const Bits *v1, const Bits *v2) {
 
 static FAST_CALL bool same(const Obj *o1, const Obj *o2) {
     const Bits *v1 = (const Bits *)o1, *v2 = (const Bits *)o2;
-    if (o1->obj != o2->obj || ((v1->len ^ v2->len) | (v1->bits ^ v2->bits)) != 0) return false;
+    if (o1->obj != o2->obj || v1->len != v2->len || v1->bits != v2->bits) return false;
     switch (v1->len) {
     case 0: return true;
     case -1:
@@ -288,7 +288,7 @@ static MUST_CHECK Obj *repr(Obj *o1, linepos_t UNUSED(epoint), size_t maxsize) {
 static MUST_CHECK Error *hash(Obj *o1, int *hs, linepos_t UNUSED(epoint)) {
     Bits *v1 = (Bits *)o1;
     size_t l;
-    int h;
+    unsigned int h;
 
     switch (v1->len) {
     case ~1: *hs = (v1->bits ^ ~v1->data[0]) & ((~0U) >> 1); return NULL;
@@ -303,7 +303,7 @@ static MUST_CHECK Error *hash(Obj *o1, int *hs, linepos_t UNUSED(epoint)) {
     }
     if (v1->len < 0) {
         l = (size_t)~v1->len;
-        h = -1;
+        h = ~0U;
         while ((l--) != 0) {
             h -= v1->data[l];
         }
@@ -316,8 +316,8 @@ static MUST_CHECK Error *hash(Obj *o1, int *hs, linepos_t UNUSED(epoint)) {
     }
     h ^= v1->bits;
     h &= ((~0U) >> 1);
-    if (v1->data != v1->u.val) v1->u.hash = h;
-    *hs = h;
+    if (v1->data != v1->u.val) v1->u.hash = (int)h;
+    *hs = (int)h;
     return NULL;
 }
 

bytesobj.c
@@ -246,10 +246,10 @@ static uint8_t *z85_encode(uint8_t *dest, const uint8_t *src, size_t len) {
 
         for (j = 4; j > 0; j--) {
             uint32_t divided = tmp / 85;
-            dest[j] = z85[tmp - divided * 85];
+            dest[j] = (uint8_t)z85[tmp - divided * 85];
             tmp = divided;
         }
-        dest[j] = z85[tmp];
+        dest[j] = (uint8_t)z85[tmp];
         dest += 5;
     }
     return dest;
@@ -365,8 +365,8 @@ static MUST_CHECK Error *hash(Obj *o1, int *hs, linepos_t UNUSED(epoint)) {
     while ((l--) != 0) h = (1000003 * h) ^ *s2++;
     h ^= (unsigned int)v1->len;
     h &= ((~0U) >> 1);
-    if (v1->data != v1->u.val) v1->u.s.hash = h;
-    *hs = h;
+    if (v1->data != v1->u.val) v1->u.s.hash = (int)h;
+    *hs = (int)h;
     return NULL;
 }
 
@@ -462,7 +462,7 @@ MUST_CHECK Obj *bytes_from_z85str(const uint8_t *s, size_t *ln, linepos_t epoint
     sz = (i > 1) ? (j + i - 1) : j;
     v = new_bytes2(sz);
     if (v == NULL) return (Obj *)new_error_mem(epoint);
-    v->len = sz;
+    v->len = (ssize_t)sz;
     s = z85_decode(v->data, s + 1, j);
     if (i > 1) {
         uint8_t tmp2[4], tmp[5] = {'0', '0', '0', '0', '0'};
@@ -592,7 +592,7 @@ MUST_CHECK Obj *bytes_calc1(Oper_types op, unsigned int val) {
     default: return (Obj *)bytes_from_u8(val);
     case O_HWORD: val >>= 8; /* fall through */
     case O_WORD: return (Obj *)bytes_from_u16(val);
-    case O_BSWORD: return (Obj *)bytes_from_u16((uint8_t)(val >> 8) | (uint16_t)(val << 8));
+    case O_BSWORD: return (Obj *)bytes_from_u16(((uint16_t)val >> 8) | (uint16_t)(val << 8));
     }
 }
 
@@ -1049,27 +1049,27 @@ static MUST_CHECK Obj *calc1(oper_t op) {
     switch (op->op->op) {
     case O_BANK:
         if (v1->len > 2) return (Obj *)bytes_from_u8(v1->data[2]);
-        if (v1->len < ~2) return (Obj *)bytes_from_u8(~v1->data[2]);
+        if (v1->len < ~2) return (Obj *)bytes_from_u8(~(unsigned int)v1->data[2]);
         return (Obj *)bytes_from_u8((v1->len < 0) ? ~0U : 0);
     case O_HIGHER:
         if (v1->len > 1) return (Obj *)bytes_from_u8(v1->data[1]);
-        if (v1->len < ~1) return (Obj *)bytes_from_u8(~v1->data[1]);
+        if (v1->len < ~1) return (Obj *)bytes_from_u8(~(unsigned int)v1->data[1]);
         return (Obj *)bytes_from_u8((v1->len < 0) ? ~0U : 0);
     case O_LOWER:
         if (v1->len > 0) return (Obj *)bytes_from_u8(v1->data[0]);
-        if (v1->len < ~0) return (Obj *)bytes_from_u8(~v1->data[0]);
+        if (v1->len < ~0) return (Obj *)bytes_from_u8(~(unsigned int)v1->data[0]);
         return (Obj *)bytes_from_u8((v1->len < 0) ? ~0U : 0);
     case O_HWORD:
         if (v1->len > 2) return (Obj *)bytes_from_u16(v1->data[1] + ((unsigned int)v1->data[2] << 8));
         if (v1->len > 1) return (Obj *)bytes_from_u16(v1->data[1]);
         if (v1->len < ~2) return (Obj *)bytes_from_u16(~(v1->data[1] + ((unsigned int)v1->data[2] << 8)));
-        if (v1->len < ~1) return (Obj *)bytes_from_u16(~v1->data[1]);
+        if (v1->len < ~1) return (Obj *)bytes_from_u16(~(unsigned int)v1->data[1]);
         return (Obj *)bytes_from_u16((v1->len < 0) ? ~0U : 0);
     case O_WORD:
         if (v1->len > 1) return (Obj *)bytes_from_u16(v1->data[0] + ((unsigned int)v1->data[1] << 8));
         if (v1->len > 0) return (Obj *)bytes_from_u16(v1->data[0]);
         if (v1->len < ~1) return (Obj *)bytes_from_u16(~(v1->data[0] + ((unsigned int)v1->data[1] << 8)));
-        if (v1->len < ~0) return (Obj *)bytes_from_u16(~v1->data[0]);
+        if (v1->len < ~0) return (Obj *)bytes_from_u16(~(unsigned int)v1->data[0]);
         return (Obj *)bytes_from_u16((v1->len < 0) ? ~0U : 0);
     case O_BSWORD:
         if (v1->len > 1) return (Obj *)bytes_from_u16(v1->data[1] + ((unsigned int)v1->data[0] << 8));
@@ -1205,7 +1205,7 @@ static inline MUST_CHECK Obj *repeat(oper_t op) {
         v->len = 0;
         while ((rep--) != 0) {
             memcpy(s + v->len, v1->data, len1);
-            v->len += len1;
+            v->len += (ssize_t)len1;
         }
         if (v1->len < 0) v->len = ~v->len;
         return &v->v;

encoding.c
@@ -72,7 +72,7 @@ static FAST_CALL int encoding_compare(const struct avltree_node *aa, const struc
 
 static void escape_free(void *e) {
     struct escape_s *esc;
-    size_t i = (const uint8_t *)e - identmap;
+    size_t i = (size_t)((const uint8_t *)e - identmap);
     if (i < 256) return;
     esc = (struct escape_s *)e;
     if (esc->data != esc->val) free(esc->data);
@@ -266,7 +266,7 @@ bool new_escape(struct encoding_s *enc, const str_t *v, Obj *val, linepos_t epoi
     }
     *b2 = b;
     if (i == 1) {
-        size_t j = (const uint8_t *)b - identmap;
+        size_t j = (size_t)((const uint8_t *)b - identmap);
         if (j < 256) {
             return b != (struct escape_s *)(identmap + tmp.val[0]);
         }
@@ -359,10 +359,10 @@ next:
         size_t len = encoder->len - encoder->i;
         struct escape_s *e = (struct escape_s *)ternary_search(encoding->escapes, encoder->data + encoder->i, &len);
         if (e != NULL) {
-            size_t i = (const uint8_t *)e - identmap;
+            size_t i = (size_t)((const uint8_t *)e - identmap);
             if (i < 256) {
                 encoder->i += len;
-                return i;
+                return (int)i;
             }
             if (e->pass >= pass || !fixeddig || e->pass == pass - 1) {
                 encoder->i += len;

error.c
@@ -100,7 +100,7 @@ static FAST_CALL int duplicate_compare(const struct avltree_node *aa, const stru
     const struct errorentry_s *b = cavltree_container_of(bb, struct errorentry_s, node);
     const uint8_t *aerr, *berr;
 
-    if (a->severity != b->severity) return a->severity - b->severity;
+    if (a->severity != b->severity) return (int)a->severity - (int)b->severity;
     if (a->file_list != b->file_list) return a->file_list > b->file_list ? 1 : -1;
     if (a->error_len != b->error_len) return a->error_len > b->error_len ? 1 : -1;
     if (a->epoint.line != b->epoint.line) return a->epoint.line > b->epoint.line ? 1 : -1;
@@ -163,7 +163,7 @@ static void error_extend(void) {
     bool dir;
     if (data == NULL) err_msg_out_of_memory2();
     dir = data >= error_list.data;
-    diff = dir ? (data - error_list.data) : (error_list.data - data);
+    diff = dir ? (size_t)(data - error_list.data) : (size_t)(error_list.data - data);
     error_list.data = data;
     if (diff == 0) return;
     for (pos = 0; pos < error_list.header_pos; pos = ALIGN(pos + (sizeof *err) + err->line_len + err->error_len)) {
@@ -1108,7 +1108,7 @@ static const char * const opr_names[ADR_LEN] = {
     "", /* ADR_BIT_ZP_REL */
 };
 
-void err_msg_address_mismatch(int a, int b, linepos_t epoint) {
+void err_msg_address_mismatch(unsigned int a, unsigned int b, linepos_t epoint) {
     new_error_msg2(diagnostic_errors.altmode, epoint);
     adderror("using ");
     adderror(opr_names[a]);
@@ -1156,7 +1156,7 @@ static const char * const order_suffix[4] = {
 
 void err_msg_missing_argument(linepos_t epoint, size_t n) {
     char msg2[4];
-    int i = n % 10;
+    unsigned int i = n % 10;
     bool more = new_error_msg(SV_ERROR, &((const struct file_listnode_s *)current_file_list)->parent->flist, &current_file_list->epoint);
     msg2[0] = n + '1';
     memcpy(msg2 + 1, order_suffix[i < 4 ? i : 3], 3);

error.h
@@ -72,7 +72,7 @@ extern void err_msg_not_defined2a(int32_t, struct Namespace *, bool, linepos_t);
 extern void err_msg_symbol_case(const struct str_t *, const struct Label *, linepos_t);
 extern void err_msg_symbol_case2(const struct Symbol *, const struct Symbol *);
 extern void err_msg_macro_prefix(linepos_t);
-extern void err_msg_address_mismatch(int, int, linepos_t);
+extern void err_msg_address_mismatch(unsigned int, unsigned int, linepos_t);
 extern void err_msg_file(Error_types, const char *, linepos_t);
 extern void err_msg_output(const struct Error *);
 extern void err_msg_output_and_destroy(struct Error *);

file.c
@@ -213,7 +213,7 @@ static bool portability(const str_t *name, linepos_t epoint) {
         pos = (const uint8_t *)memchr(name->data, *c, name->len);
         if (pos == NULL) continue;
         epoint2.line = epoint->line;
-        epoint2.pos = interstring_position(epoint, name->data, pos - name->data);
+        epoint2.pos = interstring_position(epoint, name->data, (size_t)(pos - name->data));
         err_msg2(ERROR__RESERVED_CHR, name, &epoint2);
         return false;
     }
@@ -442,7 +442,7 @@ static struct file_s *command_line = NULL;
 static struct file_s *lastfi = NULL;
 static struct ubuff_s last_ubuff;
 static uint16_t curfnum;
-struct file_s *openfile(const char *name, const char *base, int ftype, const str_t *val, linepos_t epoint) {
+struct file_s *openfile(const char *name, const char *base, unsigned int ftype, const str_t *val, linepos_t epoint) {
     struct file_s *tmp;
     char *s;
     if (lastfi == NULL) {
@@ -456,7 +456,7 @@ struct file_s *openfile(const char *name, const char *base, int ftype, const str
         lastfi->name = name;
         n.data = (const uint8_t *)name;
         n.len = strlen(name);
-        lastfi->hash = (str_hash(&n) + str_hash(&lastfi->base) + lastfi->type) & ((~0U) >> 1);
+        lastfi->hash = ((unsigned int)str_hash(&n) + (unsigned int)str_hash(&lastfi->base) + lastfi->type) & ((~0U) >> 1);
         tmp = file_table_update(lastfi);
     } else {
         tmp = (command_line != NULL) ? command_line : NULL;

commit ffe477676e39b95332a128fbc6f25b85a6987173

Broke in r2428...
git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@2824 111d7451-93fa-4e81-90d4-8039f76616e9

codeobj.c
@@ -108,6 +108,10 @@ static inline address_t code_address(const Code *v1) {
     return v1->offs < 0 ? v1->addr - -(uval_t)v1->offs : v1->addr + (uval_t)v1->offs;
 }
 
+static inline address_t code_memaddress(const Code *v1) {
+    return v1->offs < 0 ? v1->memaddr - -(uval_t)v1->offs : v1->memaddr + (uval_t)v1->offs;
+}
+
 static MUST_CHECK Obj *get_code_address(const Code *v1, linepos_t epoint) {
     address_t addr2 = code_address(v1);
     address_t addr = addr2 & all_mem;
@@ -586,7 +590,7 @@ static MUST_CHECK Obj *calc2(oper_t op) {
                 str_cfcpy(&cf, &v2->name);
                 if (str_cmp(&cf, &of) == 0) {
                     if (diagnostics.case_symbol && str_cmp(&v2->name, &cf) != 0) err_msg_symbol_case(&v2->name, NULL, op->epoint2);
-                    return int_from_uval(code_address(v1) & all_mem2);
+                    return int_from_uval(code_memaddress(v1) & all_mem2);
                 }
             }
         }