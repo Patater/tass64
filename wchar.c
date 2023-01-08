a8358792c570acc763e738baa5792cd406de8085
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2023-01-08 21:28:13 +0000

    Simplify
    git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@2966 111d7451-93fa-4e81-90d4-8039f76616e9

--- wchar.c
+++ wchar.c
@@ -518,7 +518,7 @@ size_t wcrtomb(char *s, wchar_t wc, mbstate_t *UNUSED(ps)) {
     errno = EILSEQ;
     return (size_t)-1;
 }
-#elif defined __GNUC__ || _MSC_VER >= 1400 || defined __WATCOMC__
+#elif defined __GNUC__ || defined _MSC_VER || defined __WATCOMC__
 #elif __STDC_VERSION__ >= 199901L && !defined __VBCC__
 #else
 #include <errno.h>
