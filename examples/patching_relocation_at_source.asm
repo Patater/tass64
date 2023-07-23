7c722312c2e218838dacadfda165f159971ef920
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2023-07-23 06:31:28 +0000

    Whitespace...
    git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@2987 111d7451-93fa-4e81-90d4-8039f76616e9

--- examples/patching_relocation_at_source.asm
+++ examples/patching_relocation_at_source.asm
@@ -1,21 +1,21 @@
 
-		*= $1000
+                *= $1000
 
 ; Static relocation is easy, just use .logical/.here and copy the code to the
 ; already know destination for execution.
 
-		ldx #0
+                ldx #0
 _lp
-		lda inram,x
-		sta reloc,x
-		inx
-		cpx #size(reloc)
-		bcc _lp
+                lda inram,x
+                sta reloc,x
+                inx
+                cpx #size(reloc)
+                bcc _lp
 
 ; Patching it at the destination is simple as well. As all labels point to
 ; destination memory it can be directly overwritten.
 
-		sta reloc.patchme + 1
+                sta reloc.patchme + 1
 
 ; But what if the code has to be transmitted in some way to somewhere else for
 ; execution? Then the only way is to patch it before the transmission in RAM.
@@ -25,26 +25,26 @@ _lp
 ; Normally one calculates the in memory address by adjusting it with the
 ; difference:
 
-		sta inram + reloc.patchme - reloc + 1
+                sta inram + reloc.patchme - reloc + 1
 
 ; When patching many locations it's less error prone to move the calculation to
 ; a function:
 
-		sta patch(reloc.patchme) + 1
-		rts
+                sta patch(reloc.patchme) + 1
+                rts
 
 ; Function to calculate in memory address. May be a good idea to put it inside
 ; the "reloc" scope but that's not done now.
 
-patch		.sfunction _adr, inram + _adr - reloc
+patch           .sfunction _adr, inram + _adr - reloc
 
 ; Example relocated routine, compiled here but needs to execute at $2000
 
-inram		.logical $2000
-reloc		.block			; in separate scope as a reminder
-patchme		lda #0			; that labels inside are relocated
-		inc patchme+1
-		.bend
+inram           .logical $2000
+reloc           .block                  ; in separate scope as a reminder
+patchme         lda #0                  ; that labels inside are relocated
+                inc patchme+1
+                .bend
                 .here
 
 ;-----------------------
@@ -52,7 +52,7 @@ patchme		lda #0			; that labels inside are relocated
 ; relocated code into a macro and compile it twice just to get direct labels
 ; which carry the in memory address:
 
-		sta virt.patchme + 1
+                sta virt.patchme + 1
 
 ; For the virtual double compile hack the code in the macro must be written so
 ; that it compiles the same way every time!
@@ -62,24 +62,24 @@ patchme		lda #0			; that labels inside are relocated
 ; the second compile will have useless shifted addresses from there on. You
 ; have been warned.
 
-drvcode		.macro
-patchme		lda #0
-		inc patchme+1
-		.endm
+drvcode         .macro
+patchme         lda #0
+                inc patchme+1
+                .endm
 
 ; Actual in memory code relocated to $0300.
 
-inram2		.logical $300
-		#drvcode
-		.here
+inram2          .logical $300
+                #drvcode
+                .here
 
 ; Virtual compile a second time to have the right labels for patching
 
-		.virtual inram2
-virt		#drvcode
-		.endv
+                .virtual inram2
+virt            #drvcode
+                .endv
 
 ; Sanity check, not perfect but better than nothing. Change the relocation from
 ; $300 to $30 see it triggered
 
-		.cerror size(virt) != size(reloc), "Different size, how?!"
+                .cerror size(virt) != size(reloc), "Different size, how?!"
