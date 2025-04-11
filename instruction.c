dabfe104b17fa1192650f8e085fd20ebf93d9746
soci <soci@111d7451-93fa-4e81-90d4-8039f76616e9>
2025-04-11 01:07:44 +0000

    Reorder
    git-svn-id: svn://svn.code.sf.net/p/tass64/code/trunk@3199 111d7451-93fa-4e81-90d4-8039f76616e9

--- instruction.c
+++ instruction.c
@@ -769,16 +769,19 @@ MUST_CHECK Error *instruction(int prm, unsigned int w, Funcargs *vals, linepos_t
                         }
                     }
                     if (adr == 1) {
-                        if (s == NULL) s = new_star(vline + 1);
-                        if (s->pass == 0 || (uint16_t)(uval - s->addr) == 1) {
-                            if ((cnmemonic[OPR_REL] & 0x1f) == 0x10) {
+                        if ((cnmemonic[OPR_REL] & 0x1f) == 0x10) {
+                            if (s == NULL) s = new_star(vline + 1);
+                            if (s->pass == 0 || (uint16_t)(uval - s->addr) == 1) {
                                 if (diagnostics.optimize) cpu_opt_long_branch(cnmemonic[OPR_REL] | 0x100U);
                                 dump_instr(cnmemonic[OPR_REL] ^ 0x20, 1, 0, epoint);
                                 if (diagnostics.optimize) cpu_opt_long_branch(0);
                                 err = NULL;
                                 goto branchend;
                             }
-                            if (cnmemonic[OPR_REL] == 0x80 && (opcode == r65c02.opcode || opcode == w65c02.opcode)) {
+                        }
+                        if (cnmemonic[OPR_REL] == 0x80 && (opcode == r65c02.opcode || opcode == w65c02.opcode)) {
+                            if (s == NULL) s = new_star(vline + 1);
+                            if (s->pass == 0 || (uint16_t)(uval - s->addr) == 1) {
                                 dump_instr(0x82, 1, 0, epoint);
                                 err = NULL;
                                 goto branchend;
@@ -786,9 +789,9 @@ MUST_CHECK Error *instruction(int prm, unsigned int w, Funcargs *vals, linepos_t
                         }
                     }
                     if (adr == 2 && (opcode == c65ce02.opcode || opcode == c4510.opcode || opcode == c45gs02.opcode)) {
-                        if (s == NULL) s = new_star(vline + 1);
-                        if (s->pass == 0 || (uint16_t)(uval - s->addr) == 2) {
-                            if ((cnmemonic[OPR_REL] & 0x1f) == 0x10) {
+                        if ((cnmemonic[OPR_REL] & 0x1f) == 0x10) {
+                            if (s == NULL) s = new_star(vline + 1);
+                            if (s->pass == 0 || (uint16_t)(uval - s->addr) == 2) {
                                 if (diagnostics.optimize) cpu_opt_long_branch(cnmemonic[OPR_REL] | 0x100U);
                                 dump_instr(cnmemonic[OPR_REL] ^ 0x23, 2, 0, epoint);
                                 if (diagnostics.optimize) cpu_opt_long_branch(0);
